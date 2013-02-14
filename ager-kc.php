46fe4b7fbb75f46d0dc3665f0f6099c8802fa63e
mpelzsherman <mpelzsherman@gmail.com>
2013-02-14 00:11:10 -0500

    Added aging support for kevincohen.com, with a bit of refactoring and improved logs.

---
+++ ager-kc.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once 'config-kc.php';
+require_once 'util.php';
+require_once 'lib_ager.inc';
+
+date_default_timezone_set('America/New_York');
+// workaround for default security settings?
+ini_set('allow_url_fopen', 'On');
+
+/***********************************/
+/** set your account details here **/
+/***********************************/
+$accountId = 1245985;
+$clientFolderId = 16941;
+
+$prospectList = 23140;
+$purchasedList = 23139;
+$deadList = 32558;
+
+ageProspects($accountId, $clientFolderId, $prospectList, $purchasedList, $deadList);
+
+?>
---
+++ config-kc.php
@@ -0,0 +1,10 @@
+<?php
+
+$GLOBALS['config'] = array(
+    'apiUrl'   => 'https://app.icontact.com/icp',
+    'username' => 'kevincohenconsulting@gmail.com',
+    'password' => 'FooBar123',
+    'appId'    => 'RbiHWEjlYFgX7DhBwuGWGtESwMTRrQq9',
+);
+
+?>
